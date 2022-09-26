5fc76278d783c06d186d449da024fcfbfb27a731
Chetan Kumbargoudar <chetan.kumbargoudar@continental-corporation.com>
2022-09-26 19:17:17 +0530

    newtst

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ const express = require( "express" );
 const app = express();
 const port = 8080; // default port to listen
 
-// define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
-
+app.get("/test", (_req, res) =>  {
+    res.status(200).send("Hello world")
+  })
+module.exports = app;
 // start the Express server
 app.listen( port, () => {
     console.log( `server started at http://localhost:${ port }` );
